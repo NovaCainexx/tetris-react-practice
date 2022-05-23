9b167c7945cbbc6e6e44c8dfc3d73312ac48ceb5
Lisa Nolte <lisan@bbd.co.za>
2022-05-23 15:25:36 +0200

    game over implemented

--- tetris/src/components/Controls.js
+++ tetris/src/components/Controls.js
@@ -4,26 +4,43 @@ import { moveDown, moveLeft, moveRight, rotate } from '../actions'
 
 export default function Controls(props) {
     const dispatch = useDispatch()
-    const isRunning = useSelector((state) => state.isRunning)
+    const isRunning = useSelector((state) => state.game.isRunning)
+    const gameOver = useSelector((state) => state.game.gameOver)
     return (
         <div className="controls">
             {/* left */}
-            <button className="control-button" onClick={(e) => {
-                dispatch(moveLeft())
-            }}>Left</button>
+            <button 
+                disabled={!isRunning || gameOver}
+                className="control-button" 
+                onClick={(e) => {
+                    if (!isRunning || gameOver) { return } 
+                    dispatch(moveLeft())
+                }}>Left</button>
 
             {/* right */}
-            <button className="control-button" onClick={(e) => {
+            <button 
+                disabled={!isRunning || gameOver}
+                className="control-button" 
+                onClick={(e) => {
+                    if (!isRunning || gameOver) { return }
                 dispatch(moveRight())
             }}>Right</button>
 
             {/* rotate */}
-            <button className="control-button" onClick={(e) => {
+            <button 
+                disabled={!isRunning || gameOver}
+                className="control-button" 
+                onClick={(e) => {
+                    if (!isRunning || gameOver) { return }
                 dispatch(rotate())
             }}>Rotate</button>
 
             {/* down */}
-            <button className="control-button" onClick={(e) => {
+            <button 
+                disabled={!isRunning || gameOver}
+                className="control-button" 
+                onClick={(e) => {
+                    if (!isRunning || gameOver) { return }
                 dispatch(moveDown())
             }}>Down</button>
 
